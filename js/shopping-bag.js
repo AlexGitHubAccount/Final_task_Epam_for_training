6e04af4521a6660b56379072eda85a0e3e108794
DidenDv <d.didenko86@gmail.com>
2020-03-30 21:34:21 +0300

    added count

--- js/shopping-bag.js
+++ js/shopping-bag.js
@@ -1,24 +1,49 @@
 // Function for increment amount of quantity of items inside the bag
 (function () {
 
+    const data = JSON.parse(localStorage.getItem('cart'));
+    let fullPrice = parseFloat(localStorage.getItem('goodsPrice'));
+    const totalCost = document.querySelector('.totalCost');
+
+    function changePrice(priceItem, attrBtn) {
+        if (attrBtn === "plus") {
+            fullPrice = fullPrice + priceItem;
+        } else {
+            fullPrice = fullPrice - priceItem;
+        }
+        document.querySelector('.commonPrice').innerHTML =
+            `£ ${localStorage.goodsCounter ? fullPrice.toFixed(2) : '0'}<span class="countItems"> (${localStorage.goodsCounter})</span>`;
+        setTimeout(function () {
+            totalCost.innerHTML =  `${fullPrice ? fullPrice.toFixed(2) : '£ 0'}`;
+        },0);
+    }
+
+    function removeItem(item, id) {
+        item.closest('.shopOptions').querySelector('.removeItem').click();
+        delete data[id];
+    }
+
     function countChange(event) {
         let target = event && event.target || event.srcElement;
         let btn = target.getAttribute('data-count');
+        let rel = target.closest('.shoppingBlock').getAttribute('data-block');
+        let input = target.closest('.shoppingBlock').querySelector('.quantityBag');
+        let priceItem = parseFloat(data[rel].price.slice(1));
 
         if (!btn) return;
 
         if (btn === "plus") {
             target.previousElementSibling.innerHTML++;
+            changePrice(priceItem, btn);
         }
         if (btn === "minus") {
-            target.nextElementSibling.innerHTML === "0" ?
-                target.nextElementSibling.innerText = 0 :
+            target.nextElementSibling.innerHTML === "1" ?
+                removeItem(target, rel) :
                 target.nextElementSibling.innerHTML--;
-            // target.nextElementSibling.innerHTML === "0" ?
-            //     this.removeItem.call(Shop.prototype) :
-            //     target.nextElementSibling.innerHTML--;
-
+            changePrice(priceItem, btn);
         }
+        if (data[rel]) data[rel].qw = parseInt(input.innerHTML);
+        localStorage.cart = JSON.stringify(data);
     }
 
     function initCount() {
